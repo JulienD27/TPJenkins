8efe658c5b001472a842d5ace18e842b6309a20e
jd594217 <juliendurand77140@gmail.com>
2023-03-28 14:18:53 +0200

    Initial commit

---
+++ src/main/java/Book.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Book {
+    public String title;
+
+    public String author;
+
+    public Date published;
+
+    public Book(String title, String author, Date published) {
+        this.title = title;
+        this.author = author;
+        this.published = published;
+    }
+
+
+}
